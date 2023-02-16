d47672a3d7a39f26b78ed082ef791a5d564b46d6
Vincent Zheng <zhengvincent0825@gmail.com>
2023-02-16 18:04:00 -0500

    included Vue program store and bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-createApp(App).mount('#app')
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+createApp(App).use(store).mount('#app')
---
+++ client/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+    state: {
+        currentSelectedState: null
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
+    getters: {
+
+    }
+})
