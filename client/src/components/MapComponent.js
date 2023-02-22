7f8027baa0c387c51f75db8a128b0bdeb258d073
Dylan Gold <dylangoldj@gmail.com>
2023-02-22 17:58:54 -0500

    Map render fix

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -2,12 +2,16 @@ import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
 import { useMap } from 'react-leaflet/hooks'
 import { Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'
+
+import Box from '@mui/material/Box';
 
 function Map() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <Box
+        sx={{width: 1/3}} >
+    <MapContainer  style={{ width: "100%", height: "100vh" }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={[51.505, -0.09]}>
@@ -16,6 +20,7 @@ function Map() {
         </Popup>
       </Marker>
     </MapContainer>
+    </Box>
   );
 }
 
