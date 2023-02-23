789d67ce9159320f6a93ebd243785bc69ab72f04
Vincent Zheng <zhengvincent0825@gmail.com>
2023-02-22 19:35:08 -0500

    adding state outlines

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -1,22 +1,15 @@
-import { MapContainer } from 'react-leaflet/MapContainer'
-import { TileLayer } from 'react-leaflet/TileLayer'
-import { useMap } from 'react-leaflet/hooks'
-import { Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-import Box from '@mui/material/Box';
+import mapOverview from '../assets/geoJSON';
 
 function Map() {
   return (
-    <MapContainer  style={{ width: "100%", height: "50vh" }} center={[37.6, -95.665]} zoom={4} scrollWheelZoom={false}>
+    <MapContainer  style={{ width: "100%", height: "50vh" }} center={[37.6, -96]} zoom={4} scrollWheelZoom={false}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <GeoJSON data={mapOverview} />
     </MapContainer>
   );
 }
