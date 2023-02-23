1c5726463ac3a1dcfc7b5aa90e1988ebb23cb938
Vincent Zheng <zhengvincent0825@gmail.com>
2023-02-22 22:30:27 -0500

    interactable map

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -2,16 +2,19 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedState } from '../reducers/MapReducer';
 
-//Leaflet Imports
+// Leaflet Imports
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import mapOverview from '../assets/geoJSON';
 
 // Mui Imports
-import Box from '@mui/material/Box';
+import { useRef } from 'react';
 
 
 function Map() {
+  const mapRef = useRef(null);
+  const geojsonRef = useRef(null);
+
   // Get map from MapReducer
   const map = useSelector(state => state.map);
   const dispatch = useDispatch();
@@ -24,25 +27,25 @@ function Map() {
     return feature.properties.name;
   }
 
-  // var geojson
-
   var onEachFeature = (feature, layer) => {
     // Bind click to zoom in on state.
     layer.on({
-        //On click, change selected state.  
+      // On click, change selected state.  
       'click': function (event) {
-        console.log(getName(feature))
         dispatch(setSelectedState(getName(feature)))
+
+        const centerCoords = {"Ohio": [40.4173, -82.9071], "Nevada": [38.8026, -116.4194], "Illinois": [40.6331, -89.3985]};
+        if (centerCoords[getName(feature)] != null)
+          mapRef.current.flyTo(centerCoords[getName(feature)], 6);
       },
-      //On hover, highlight
+
+      // On hover, highlight
       'mouseover' : highlightFeature,
-
-      //'mouseout' : resetHighlight TODO
-
-
+      'mouseout' : resetHighlight
     });
   }
 
+  // Highlights the state that cursur is currently over.
   function highlightFeature(e) {
     var layer = e.target;
 
@@ -56,16 +59,17 @@ function Map() {
     layer.bringToFront();
   }
 
-  // function resetHighlight(e) { TODO
-  //   geojson.resetStyle(e.target);
-  // }
+  // Resets the style currently set to the state.
+  function resetHighlight(e) {
+    geojsonRef.current.resetStyle(e.target);
+  }
 
   return (
-    <MapContainer  style={{ width: "100%", height: "50vh" }} center={[37.6, -96]} zoom={5} scrollWheelZoom={false}>
+    <MapContainer ref={mapRef} style={{ width: "100%", height: "50vh" }} center={[37.6, -96]} zoom={5} scrollWheelZoom={false}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <GeoJSON data={mapOverview} style={getColor} onEachFeature={onEachFeature} />
+      <GeoJSON ref={geojsonRef} data={mapOverview} style={getColor} onEachFeature={onEachFeature} />
     </MapContainer>
   );
 }
