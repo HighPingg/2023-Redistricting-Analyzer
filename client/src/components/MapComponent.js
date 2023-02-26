4e6e84b1aaa6105fc97d878c0b4142132c059993
Vincent Zheng <zhengvincent0825@gmail.com>
2023-02-26 00:04:31 -0500

    made map interactable with districts

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -5,10 +5,11 @@ import { setSelectedState } from '../reducers/MapReducer';
 // Leaflet Imports
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import mapOverview from '../assets/geoJSON';
 
 // Mui Imports
 import { useRef } from 'react';
+import { Box, IconButton } from '@mui/material';
+import ReplayIcon from '@mui/icons-material/Replay';
 
 
 function Map() {
@@ -19,30 +20,48 @@ function Map() {
   const map = useSelector(state => state.map);
   const dispatch = useDispatch();
 
-  var getColor = (feature) => {
-    return {color: feature.properties.color, weight: 1};
+  var getColor = (feature, selectedState) => {
+    console.log(feature)
+
+    if (selectedState === null) {
+      return {color: feature.properties.color, weight: 1};
+    } else {
+      return {weight: 1};
+    }
   }
 
   var getName = (feature) => {
     return feature.properties.name;
   }
 
-  var onEachFeature = (feature, layer) => {
-    // Bind click to zoom in on state.
-    layer.on({
-      // On click, change selected state.  
-      'click': function (event) {
-        dispatch(setSelectedState(getName(feature)))
+  // Apply interactions to the map polygons
+  var onEachFeature = (feature, layer, selectedState) => {
+    if (selectedState === null) {
+      // Bind click to zoom in on state.
+      layer.on({
+        // On click, change selected state.  
+        'click': function (event) {
+          dispatch(setSelectedState(getName(feature)))
 
-        const centerCoords = {"Ohio": [40.4173, -82.9071], "Nevada": [38.8026, -116.4194], "Illinois": [40.6331, -89.3985]};
-        if (centerCoords[getName(feature)] != null)
-          mapRef.current.flyTo(centerCoords[getName(feature)], 6);
-      },
+          const centerCoords = {"Ohio": [40.4173, -82.9071], "Nevada": [38.8026, -116.4194], "Illinois": [40.6331, -89.3985]};
+          if (centerCoords[getName(feature)] != null)
+            mapRef.current.flyTo(centerCoords[getName(feature)], 6);
+        },
 
-      // On hover, highlight
-      'mouseover' : highlightFeature,
-      'mouseout' : resetHighlight
-    });
+        // On hover, highlight
+        'mouseover' : highlightFeature,
+        'mouseout' : resetHighlight
+      });
+    } else {
+      // When focused on a state, we want to instead display district info.
+    }
+  }
+
+  // Reset the map to overview.
+  var resetMap = (event) => {
+    dispatch(setSelectedState(null));
+
+    mapRef.current.flyTo([37.6, -96], 5);
   }
 
   // Highlights the state that cursur is currently over.
@@ -65,12 +84,28 @@ function Map() {
   }
 
   return (
-    <MapContainer ref={mapRef} style={{ width: "100%", height: "50vh" }} center={[37.6, -96]} zoom={5} scrollWheelZoom={false}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <GeoJSON ref={geojsonRef} data={mapOverview} style={getColor} onEachFeature={onEachFeature} />
-    </MapContainer>
+    <Box>
+      {
+        // Hide reset button when no state is selected.
+        map.selectedState !== null && <IconButton style={{left: '10px', top: '10px', position: 'absolute', zIndex: 1}}
+                                                  onClick={resetMap}
+                                                >
+                                          <ReplayIcon style={{color: 'black'}}/>
+                                      </IconButton>
+      }
+
+      <MapContainer zoomControl={false} ref={mapRef} style={{ width: "100%", height: "50vh", zIndex: 0 }} center={[37.6, -96]} zoom={5} scrollWheelZoom={false}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <GeoJSON key={map.selectedState}
+                ref={geojsonRef}
+                data={map.currentGeoJSON}
+                style={(feature) => getColor(feature, map.selectedState)}
+                onEachFeature={(feature, layer) => onEachFeature(feature, layer, map.selectedState)}
+                />
+      </MapContainer>
+    </Box>
   );
 }
 
--- client/src/reducers/MapReducer.js
+++ client/src/reducers/MapReducer.js
@@ -1,19 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import mapOverview from '../assets/geoJSON';
+import illinois21Map from '../assets/illinois_21';
+import ohio21Map from '../assets/ohio_21';
+import nevada21Map from '../assets/nevada_21';
+
 const initialState = {
-    selectedState: null
+    selectedState: null,
+    currentGeoJSON: mapOverview
 }
 
 export const mapReducer = createSlice({
     name: 'map', 
     initialState,
-    reducers:{
+    reducers: {
         setSelectedState:(state, action) => {
             state.selectedState = action.payload
+
+            switch (action.payload) {
+                case "Illinois":
+                    state.currentGeoJSON = illinois21Map;
+                    break;
+                
+                case "Ohio":
+                    state.currentGeoJSON = ohio21Map;
+                    break;
+
+                case "Nevada":
+                    state.currentGeoJSON = nevada21Map;
+                    break;
+            
+                default:
+                    state.currentGeoJSON = mapOverview;
+                    break;
+            }
         }
     }
-    }
-)
+})
 
 export const {setSelectedState} = mapReducer.actions
 
