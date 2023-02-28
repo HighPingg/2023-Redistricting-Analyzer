43f26560599790508ff62be3bdf6a99720925776
kevcheng1 <kevin.cheng.1@stonybrook.edu>
2023-02-27 22:14:07 -0500

    Added in Incumbencies Table

--- client/src/components/TableComponent.js
+++ client/src/components/TableComponent.js
@@ -1,41 +1,125 @@
 // Redux Imports
-import { useSelector } from 'react-redux';
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
-function createData(district, incumbent, result, geovar, popvar) {
-    return { district, incumbent, result, geovar, popvar };
-  }
+// Imports for graphing util.
+import Chart from 'react-apexcharts';
 
-const rows = [
-    createData('')
-];
+// for future porting of data
+// function createData(district, incumbent, result, geovar, popvar) {
+//     return { district, incumbent, result, geovar, popvar };
+//   }
 
-function TableComponent(){
+// const rows = [
+//     createData('')
+// ];
 
-    let dataPoints = [
-        { label: "Bread",  y: [179, 256, 300, 418, 274] },
-        { label: "Cake",  y: [252, 346, 409, 437, 374.5] },
-        { label: "Biscuit",  y: [236, 281.5, 336.5, 428, 313] },
-        { label: "Doughnut",  y: [340, 382, 430, 452, 417] },
-        { label: "Pancakes",  y: [194, 224.5, 342, 384, 251] },
-        { label: "Bagels",  y: [241, 255, 276.5, 294, 274.5] }
-    ]
+function TableComponent() {
+    // Fake data 
+    const series = [{
+        name: 'Republican',
+        data: [6, 0]
+      }, {
+        name: 'Democratic',
+        data: [3, 0]
+      }, {
+        name: 'Open',
+        data: [0, 1]
+      }];
 
-    return(
+      const options = {
+        chart: {
+            type: 'bar',
+            height: 350,
+            stacked: true,
+          },
+          plotOptions: {
+            bar: {
+              horizontal: true,
+              dataLabels: {
+                total: {
+                  enabled: true,
+                  offsetX: 0,
+                  style: {
+                    fontSize: '13px',
+                    fontWeight: 900
+                  }
+                }
+              }
+            },
+          },
+          stroke: {
+            width: 1,
+            colors: ['#fff']
+          },
+          title: {
+            text: 'Incumbencies'
+          },
+          xaxis: {
+            categories: ["Incumbent", "Open"],
+            labels: {
+              formatter: function (val) {
+                return val + "K"
+              }
+            }
+          },
+          yaxis: {
+            title: {
+              text: undefined
+            },
+          },
+          tooltip: {
+            y: {
+              formatter: function (val) {
+                return val + "K"
+              }
+            }
+          },
+          fill: {
+            opacity: 1
+          },
+          legend: {
+            position: 'top',
+            horizontalAlign: 'left',
+            offsetX: 40
+          }
+      };
+      return (
         <div>
-
-            Put table here
-
+            <div>
+                Population Density: 198.2 <br></br>
+                District Plans: 14 <br></br>
+                Incumbents Predicted to Win: 0 <br></br>
+                Average Geographic Variation: 0 <br></br>
+                Population Variation in Incumbent Districts: 0 <br></br>
+            </div>
+            <div>
+                <Chart options={options} type="bar" series={series} width="100%" />
+            </div>
         </div>
 
+      );
+    }
 
-    );
-}
-export default Table;
+export default TableComponent;
+
+
+ // let dataPoints = [
+    //     { label: "Bread",  y: [179, 256, 300, 418, 274] },
+    //     { label: "Cake",  y: [252, 346, 409, 437, 374.5] },
+    //     { label: "Biscuit",  y: [236, 281.5, 336.5, 428, 313] },
+    //     { label: "Doughnut",  y: [340, 382, 430, 452, 417] },
+    //     { label: "Pancakes",  y: [194, 224.5, 342, 384, 251] },
+    //     { label: "Bagels",  y: [241, 255, 276.5, 294, 274.5] }
+    // ]
+
+    // return(
+    //     <div>
+    //         Population Density: 
+    //         District Plans: 
+    //         Incumbents Predicted to Win: 
+    //         Average Geographic Variation: 
+    //         Population Variation in Incumbent Districts: 
+
+
+
+    //     </div>
