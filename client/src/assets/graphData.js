25388572ec26b311a470571b43379921b984d681
Nazif Mahamud <nazif.mahamud1213@gmail.com>
2023-02-27 23:08:13 -0500

    Added all the other candidates so we can add them to table

--- client/src/assets/graphData.js
+++ client/src/assets/graphData.js
@@ -11,91 +11,136 @@ var graphingOverview = {
                     district: 1,
                     winner: "Greg Landsman",
                     house: "Democratic",
-                    votes: 151418
+                    votes: 151418,
+                    loser: ["Steve Chabot"],
+                    loserhouse: ["Republican"],
+                    loservotes: [137213]
                 },
                 {
                     district: 2,
                     winner: "Brad Wenstrup",
                     house: "Republican",
-                    votes: 188300
+                    votes: 188300,
+                    loser: ["Samantha Meadows"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [64329]
                 },
                 {
                     district: 3,
                     winner: "Joyce Beatty",
                     house: "Democratic",
-                    votes: 174846
+                    votes: 174846,
+                    loser: ["Lee Stahley"],
+                    loserhouse: ["Republican"],
+                    loservotes: [75335]
                 },
                 {
                     district: 4,
                     winner: "Jim Jordan",
                     house: "Republican",
-                    votes: 196655
+                    votes: 196655,
+                    loser: ["Tamie Wilson"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [87106]
                 },
                 {
                     district: 5,
                     winner: "Bob Latta",
                     house: "Republican",
-                    votes: 184205
+                    votes: 184205,
+                    loser: ["Craig Swartz"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [90614]
                 },
                 {
                     district: 6,
                     winner: "Bill Johnson",
                     house: "Republican",
-                    votes: 186965
+                    votes: 186965,
+                    loser: ["Louis Lyras"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [89086]
                 },
                 {
                     district: 7,
                     winner: "Max Miller",
                     house: "Republican",
-                    votes: 164904
+                    votes: 164904,
+                    loser: ["Matthew Diemer"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [132548]
                 },
                 {
                     district: 8,
                     winner: "Warren Davidson",
                     house: "Republican",
-                    votes: 177132
+                    votes: 177132,
+                    loser: ["Vanessa Enoch"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [95829]
                 },
                 {
                     district: 9,
                     winner: "Marcy Kaptur",
                     house: "Democratic",
-                    votes: 147512
+                    votes: 147512,
+                    loser: ["J.R. Majewski"],
+                    loserhouse: ["Republican"],
+                    loservotes: [113363]
                 },
                 {
                     district: 10,
                     winner: "Mike Turner",
                     house: "Republican",
-                    votes: 161792
+                    votes: 161792,
+                    loser: ["David Esrati"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [98383]
                 },
                 {
                     district: 11,
                     winner: "Shontel Brown",
                     house: "Democratic",
-                    votes: 162154
+                    votes: 162154,
+                    loser: ["Eric Brewer"],
+                    loserhouse: ["Republican"],
+                    loservotes: [46862]
                 },
                 {
                     district: 12,
                     winner: "Troy Balderson",
                     house: "Republican",
-                    votes: 187322
+                    votes: 187322,
+                    loser: ["Amy Rippel-Elton"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [82101]
                 },
                 {
                     district: 13,
                     winner: "Emilia Sykes",
                     house: "Democratic",
-                    votes: 146621
+                    votes: 146621,
+                    loser: ["Madison Gesiotto Gilbert"],
+                    loserhouse: ["Republican"],
+                    loservotes: [132181]
                 },
                 {
                     district: 14,
                     winner: "David Joyce",
                     house: "Republican",
-                    votes: 180251
+                    votes: 180251,
+                    loser: ["Matt Kilboy"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [111155]
                 },
                 {
                     district: 15,
                     winner: "Mike Carey",
                     house: "Republican",
-                    votes: 140012
+                    votes: 140012,
+                    loser: ["Matt Gary JosephsonKilboy"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [104613]
                 }
             ],
             graphTitle:"Ensemble Data for Ohio",
@@ -140,25 +185,37 @@ var graphingOverview = {
                     district: 1,
                     winner: "Dina Titus",
                     house: "Democratic",
-                    votes: 114916
+                    votes: 114916,
+                    loser: ["Mark Robertson","Ken Cavanaugh"],
+                    loserhouse: ["Republican", "Other"],
+                    loservotes: [102355, 5502]
                 },
                 {
                     district: 2,
                     winner: "Mark Amodei",
                     house: "Republican",
-                    votes: 181689
+                    votes: 181689,
+                    loser: ["Elizabeth Krause","Russell Best", "Darryl Baber"],
+                    loserhouse: ["Democratic", "Other", "Other"],
+                    loservotes: [115186, 4076, 3351]
                 },
                 {
                     district: 3,
                     winner: "Susie Lee",
                     house: "Democratic",
-                    votes: 130237
+                    votes: 130237,
+                    loser: ["April Becker"],
+                    loserhouse: ["Republican"],
+                    loservotes: [120102]
                 },
                 {
                     district: 4,
                     winner: "Steven Horsford",
                     house: "Democratic",
-                    votes: 115836
+                    votes: 115836,
+                    loser: ["Sam Peters"],
+                    loserhouse: ["Republican"],
+                    loservotes: [105037]
                 }
             ],
             graphTitle:"Ensemble Data for Nevada",
@@ -202,103 +259,154 @@ var graphingOverview = {
                     district: 1,
                     winner: "Jonathan Jackson",
                     house: "Democratic",
-                    votes: 151468
+                    votes: 151468,
+                    loser: ["Eric Carlson"],
+                    loserhouse: ["Republican"],
+                    loservotes: [77146]
                 },
                 {
                     district: 2,
                     winner: "Robin Kelly",
                     house: "Democratic",
-                    votes: 137854
+                    votes: 137854,
+                    loser: ["Thomas Lynch"],
+                    loserhouse: ["Republican"],
+                    loservotes: [68369]
                 },
                 {
                     district: 3,
                     winner: "Delia Ramirez",
                     house: "Democratic",
-                    votes: 111799
+                    votes: 111799,
+                    loser: ["Justin Burau"],
+                    loserhouse: ["Republican"],
+                    loservotes: [55063]
                 },
                 {
                     district: 4,
                     winner: "Chuy Garcia",
                     house: "Democratic",
-                    votes: 87987
+                    votes: 87987,
+                    loser: ["James Falakos", "Edward Hershey"],
+                    loserhouse: ["Republican", "Other"],
+                    loservotes: [36960, 4435]
                 },
                 {
                     district: 5,
                     winner: "Mike Quigley",
                     house: "Democratic",
-                    votes: 172040
+                    votes: 172040,
+                    loser: ["Tommy Hanson", "Jerico Matias Cruz"],
+                    loserhouse: ["Republican", "Other"],
+                    loservotes: [77185, 4135]
                 },
                 {
                     district: 6,
                     winner: "Sean Casten",
                     house: "Democratic",
-                    votes: 148279
+                    votes: 148279,
+                    loser: ["Keith Pekau"],
+                    loserhouse: ["Republican"],
+                    loservotes: [125625]
                 },
                 {
                     district: 7,
                     winner: "Danny Davis",
                     house: "Democratic",
-                    votes: 0
+                    votes: 0,
+                    loser: [],
+                    loserhouse: [],
+                    loservotes: []
                 },
                 {
                     district: 8,
                     winner: "Raja Krishnamoorthi",
                     house: "Democratic",
-                    votes: 117475
+                    votes: 117475,
+                    loser: ["Chris Dargis"],
+                    loserhouse: ["Republican"],
+                    loservotes: [89151]
                 },
                 {
                     district: 9,
                     winner: "Janice Schakowsky",
                     house: "Democratic",
-                    votes: 169009
+                    votes: 169009,
+                    loser: ["Maxwell Rice"],
+                    loserhouse: ["Republican"],
+                    loservotes: [69324]
                 },
                 {
                     district: 10,
                     winner: "Brad Schneider",
                     house: "Democratic",
-                    votes: 144489
+                    votes: 144489,
+                    loser: ["Joseph Severino"],
+                    loserhouse: ["Republican"],
+                    loservotes: [87528]
                 },
                 {
                     district: 11,
                     winner: " Bill Foster",
                     house: "Democratic",
-                    votes: 145303
+                    votes: 145303,
+                    loser: ["Catalina Lauf"],
+                    loserhouse: ["Republican"],
+                    loservotes: [113650]
                 },
                 {
                     district: 12,
                     winner: "Mike Bost",
                     house: "Republican",
-                    votes: 213438
+                    votes: 213438,
+                    loser: ["Homer Markel"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [69581]
                 },
                 {
                     district: 13,
                     winner: "Nikki Budzinski",
                     house: "Democratic",
-                    votes: 136123
+                    votes: 136123,
+                    loser: ["Regan Deering"],
+                    loserhouse: ["Republican"],
+                    loservotes: [106357]
                 },
                 {
                     district: 14,
                     winner: "Lauren Underwood",
                     house: "Democratic",
-                    votes: 127458
+                    votes: 127458,
+                    loser: ["Scoot Gryder"],
+                    loserhouse: ["Republican"],
+                    loservotes: [108045]
                 },
                 {
                     district: 15,
                     winner: "Mary Miller",
                     house: "Republican",
-                    votes: 209191
+                    votes: 209191,
+                    loser: ["Paul Lange"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [83765]
                 },
                 {
                     district: 16,
                     winner: "Darin LaHood",
                     house: "Republican",
-                    votes: 196009
+                    votes: 196009,
+                    loser: ["Elizabeth Haderlein"],
+                    loserhouse: ["Democratic"],
+                    loservotes: [98188]
                 },
                 {
                     district: 17,
                     winner: "Eric Sorensen",
                     house: "Democratic",
-                    votes: 119601
+                    votes: 119601,
+                    loser: [" Esther Joy King"],
+                    loserhouse: ["Republican"],
+                    loservotes: [111457]
                 },
             ],
             graphTitle:"Ensemble Data for Illinois",
