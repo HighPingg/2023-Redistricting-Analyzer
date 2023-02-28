349cd45728b2c802ef4d9276315161de9542e230
Vincent Zheng <zhengvincent0825@gmail.com>
2023-02-27 22:40:27 -0500

    district selectiom

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -22,7 +22,11 @@ function Map() {
   const dispatch = useDispatch();
 
   var getColor = (feature, selectedState) => {
-    return {fillColor: feature.properties.color, color:'white', weight: 1, fillOpacity: 0.8};
+    if (map.currentDistrict !== null && map.currentDistrict === feature.properties.DISTRICT) {
+      return {fillColor: 'purple', color:'white', weight: 1, fillOpacity: 1};
+    } else {
+      return {fillColor: feature.properties.color, color:'white', weight: 1, fillOpacity: 0.8};
+    }
   }
 
   var getName = (feature) => {
@@ -41,23 +45,23 @@ function Map() {
           const centerCoords = {"Ohio": [40.4173, -82.9071], "Nevada": [38.8026, -116.4194], "Illinois": [40.6331, -89.3985]};
           if (centerCoords[getName(feature)] != null)
             mapRef.current.flyTo(centerCoords[getName(feature)], 6);
-        }
+        },
+
+        // On hover, highlight
+        'mouseover' : highlightFeature,
+        'mouseout' : resetHighlight
       });
     } else {
       // On districts bind click to select district.
       layer.on({
         'click': function (event) {
           dispatch(setSelectedDistrict(feature.properties.DISTRICT));
-        }
+        },
+        // On hover, highlight
+        'mouseover' : highlightFeature,
+        'mouseout' : resetHighlight
       })
     }
-    
-    // When focused on a state, we want to instead display district info.
-    layer.on({
-      // On hover, highlight
-      'mouseover' : highlightFeature,
-      'mouseout' : resetHighlight
-    });
   }
 
   // Reset the map to overview.
@@ -110,7 +114,10 @@ function Map() {
 
   // Resets the style currently set to the state.
   function resetHighlight(e) {
-    geojsonRef.current.resetStyle(e.target);
+    console.log(map.currentDistrict)
+    if (map.currentDistrict === null || map.currentDistrict !== e.target.feature.properties.DISTRICT) {
+      geojsonRef.current.resetStyle(e.target);
+    }
 
     // Remove caption info.
     captionRef.current.innerHTML = null
@@ -166,7 +173,7 @@ function Map() {
         <GeoJSON key={map.selectedState}
                 ref={geojsonRef}
                 data={map.currentGeoJSON}
-                style={(feature) => getColor(feature, map.selectedState)}
+                style={(feature) => getColor(feature, map.currentDistrict)}
                 onEachFeature={(feature, layer) => onEachFeature(feature, layer, map.selectedState)}
                 />
       </MapContainer>
