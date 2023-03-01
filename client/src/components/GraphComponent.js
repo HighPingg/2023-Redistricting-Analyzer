c2eb73da72618fb5b74dc738542600a6502d8536
Dylan Gold <dylangoldj@gmail.com>
2023-02-28 23:39:58 -0500

    Fixing graph switching

--- client/src/components/GraphComponent.js
+++ client/src/components/GraphComponent.js
@@ -3,47 +3,132 @@ import { useSelector } from 'react-redux';
 
 // Imports for graphing util.
 import ReactApexChart from 'react-apexcharts'
+import Chart from 'react-apexcharts'
 
 function Graph(){
     // Get selected state from reducer
     const map = useSelector(state => state.map);
 
-    
+    let display = "Select a state to show more data"
     
     let options, series = null
     if (map.selectedState!= null){
-
-      series =  [
-          {
-          type: 'boxPlot',
-          data: map.currentGraphData.data
-          }
-      ];
-
-      options = {
-          chart: {
-          type: 'boxPlot',
-          height: 350
-          },
-          title: {
-          text: map.currentGraphData.graphTitle,
-          align: 'left'
-          },
-          plotOptions: {
-          boxPlot: {
-              colors: {
-              upper: '#5C4742',
-              lower: '#A5978B'
-              }
-            }
-          }
-      };
       
-    }
+      switch(map.currentDisplay){
+        case "Ensemble":
+          series =  [
+            {
+            type: 'boxPlot',
+            data: map.currentGraphData.data
+            }
+          ];
+  
+          options = {
+              chart: {
+              type: 'boxPlot',
+              height: 350
+              },
+              title: {
+              text: map.currentGraphData.graphTitle,
+              align: 'left'
+              },
+              plotOptions: {
+              boxPlot: {
+                  colors: {
+                  upper: '#5C4742',
+                  lower: '#A5978B'
+                  }
+                }
+              }
+            };
+          display = <ReactApexChart options={options} series={series} type="boxPlot" height={350} key ={map.currentDisplay}/> ;
+          break;
+
+        case "Incumbency":
+          // Fake data 
+          series = [{
+              name: 'Republican',
+              data: [6, 0]
+          }, {
+              name: 'Democratic',
+              data: [3, 0]
+          }, {
+              name: 'Open',
+              data: [0, 1]
+          }];
+
+          options = {
+              chart: {
+                  type: 'bar',
+                  height: 350,
+                  stacked: true,
+              },
+              plotOptions: {
+                  bar: {
+                  horizontal: true,
+                  dataLabels: {
+                      total: {
+                      enabled: true,
+                      offsetX: 0,
+                      style: {
+                          fontSize: '13px',
+                          fontWeight: 900
+                      }
+                      }
+                  }
+                  },
+              },
+              stroke: {
+                  width: 1,
+                  colors: ['#fff']
+              },
+              title: {
+                  text: 'Incumbencies'
+              },
+              xaxis: {
+                  categories: ["Incumbent", "Open"],
+                  labels: {
+                  formatter: function (val) {
+                      return val + "K"
+                  }
+                  }
+              },
+              yaxis: {
+                  title: {
+                  text: undefined
+                  },
+              },
+              tooltip: {
+                  y: {
+                  formatter: function (val) {
+                      return val + "K"
+                  }
+                  }
+              },
+              fill: {
+                  opacity: 1,
+                  colors: ['#F44336', '#0000FF', '#808080']
+              },
+              legend: {
+                  position: 'top',
+                  horizontalAlign: 'left',
+                  offsetX: 40
+              },
+              colors:['#F44336', '#0000FF', '#808080']
+
+          }
+          display = <Chart options={options} type="bar" series={series} width="100%" key ={map.currentDisplay}/>;
+          break;
+            
+        default:
+          display = "Select a graph type to display"
+        
+          };
+      }
       
   return (
   <div>
-    {map.currentGraphData != null ? <ReactApexChart options={options} series={series} type="boxPlot" height={350} /> : "Select a state to show more data"}
+    {display}
   </div>
   );
 }
--- client/src/components/TableComponent.js
+++ client/src/components/TableComponent.js
@@ -12,14 +12,6 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-// for future porting of data
-// function createData(district, incumbent, result, geovar, popvar) {
-//     return { district, incumbent, result, geovar, popvar };
-//   }
-
-// const rows = [
-//     createData('')
-// ];
 
 function TableComponent() {
 
@@ -32,102 +24,9 @@ function TableComponent() {
     } // Should grab all the winner data per district. The console.log should verify it
     console.log(rows);
     
-
-
-    // let options, series = null
-    // if (map.currentGraphData!= null) {
-    //     // Fake data 
-    //     series = [{
-    //         name: 'Republican',
-    //         data: [6, 0]
-    //     }, {
-    //         name: 'Democratic',
-    //         data: [3, 0]
-    //     }, {
-    //         name: 'Open',
-    //         data: [0, 1]
-    //     }];
-
-    //     options = {
-    //         chart: {
-    //             type: 'bar',
-    //             height: 350,
-    //             stacked: true,
-    //         },
-    //         plotOptions: {
-    //             bar: {
-    //             horizontal: true,
-    //             dataLabels: {
-    //                 total: {
-    //                 enabled: true,
-    //                 offsetX: 0,
-    //                 style: {
-    //                     fontSize: '13px',
-    //                     fontWeight: 900
-    //                 }
-    //                 }
-    //             }
-    //             },
-    //         },
-    //         stroke: {
-    //             width: 1,
-    //             colors: ['#fff']
-    //         },
-    //         title: {
-    //             text: 'Incumbencies'
-    //         },
-    //         xaxis: {
-    //             categories: ["Incumbent", "Open"],
-    //             labels: {
-    //             formatter: function (val) {
-    //                 return val + "K"
-    //             }
-    //             }
-    //         },
-    //         yaxis: {
-    //             title: {
-    //             text: undefined
-    //             },
-    //         },
-    //         tooltip: {
-    //             y: {
-    //             formatter: function (val) {
-    //                 return val + "K"
-    //             }
-    //             }
-    //         },
-    //         fill: {
-    //             opacity: 1,
-    //             colors: ['#F44336', '#0000FF', '#808080']
-    //         },
-    //         legend: {
-    //             position: 'top',
-    //             horizontalAlign: 'left',
-    //             offsetX: 40
-    //         },
-    //         colors:['#F44336', '#0000FF', '#808080']
-
-    //     }
-    // };
     if (map.selectedState != null){
       if (map.setSelectedDistrict == null) {
         return (
-        
-          // <div>
-          //     {map.selectedState != null ?
-          //     <div>
-          //     <div>
-          //         Population Density: 198.2 <br></br>
-          //         District Plans: 14 <br></br>
-          //         Incumbents Predicted to Win: 0 <br></br>
-          //         Average Geographic Variation: 0 <br></br>
-          //         Population Variation in Incumbent Districts: 0 <br></br>
-          //     </div>
-          //     {/* <div>
-          //         <Chart options={options} type="bar" series={series} width="100%" />
-          //     </div> */}
-          //     </div> : "Select a state to show table data"}
-          // </div>
   
           <TableContainer component={Paper} sx={{ maxHeight: '95vh' }}>
             <Table aria-aria-label='simple table'>
@@ -167,25 +66,3 @@ function TableComponent() {
     }
 
 export default TableComponent;
-
-
- // let dataPoints = [
-    //     { label: "Bread",  y: [179, 256, 300, 418, 274] },
-    //     { label: "Cake",  y: [252, 346, 409, 437, 374.5] },
-    //     { label: "Biscuit",  y: [236, 281.5, 336.5, 428, 313] },
-    //     { label: "Doughnut",  y: [340, 382, 430, 452, 417] },
-    //     { label: "Pancakes",  y: [194, 224.5, 342, 384, 251] },
-    //     { label: "Bagels",  y: [241, 255, 276.5, 294, 274.5] }
-    // ]
-
-    // return(
-    //     <div>
-    //         Population Density: 
-    //         District Plans: 
-    //         Incumbents Predicted to Win: 
-    //         Average Geographic Variation: 
-    //         Population Variation in Incumbent Districts: 
-
-
-
-    //     </div>
