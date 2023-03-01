23e9eb3cac729159eec469b0ca7c19d549bde28d
Nazif Mahamud <mamun01188@gmail.com>
2023-02-28 23:01:45 -0500

    Table header added, time for the rows

--- client/src/components/TableComponent.js
+++ client/src/components/TableComponent.js
@@ -4,7 +4,13 @@ import * as React from 'react';
 // Imports for graphing util.
 import Chart from 'react-apexcharts';
 import { useDispatch, useSelector } from 'react-redux';
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 // for future porting of data
 // function createData(district, incumbent, result, geovar, popvar) {
 //     return { district, incumbent, result, geovar, popvar };
@@ -19,6 +25,13 @@ function TableComponent() {
     const map = useSelector(state => state.map)
     const dispatch = useDispatch()
     
+    let rows = [];
+    if (map.currentGraphData != null) {
+        rows = map.currentGraphData.incumbents2022.map(x => [x.district, x.winner, x.votes, x.house]);
+    } // Should grab all the winner data per district. The console.log should verify it
+    console.log(rows);
+    
+
 
     // let options, series = null
     // if (map.currentGraphData!= null) {
@@ -96,22 +109,40 @@ function TableComponent() {
     //     }
     // };
       return (
-        <div>
-            {map.selectedState != null ?
-            <div>
-            <div>
-                Population Density: 198.2 <br></br>
-                District Plans: 14 <br></br>
-                Incumbents Predicted to Win: 0 <br></br>
-                Average Geographic Variation: 0 <br></br>
-                Population Variation in Incumbent Districts: 0 <br></br>
-            </div>
-            {/* <div>
-                <Chart options={options} type="bar" series={series} width="100%" />
-            </div> */}
-            </div> : "Select a state to show table data"}
-        </div>
-
+        // <div>
+        //     {map.selectedState != null ?
+        //     <div>
+        //     <div>
+        //         Population Density: 198.2 <br></br>
+        //         District Plans: 14 <br></br>
+        //         Incumbents Predicted to Win: 0 <br></br>
+        //         Average Geographic Variation: 0 <br></br>
+        //         Population Variation in Incumbent Districts: 0 <br></br>
+        //     </div>
+        //     {/* <div>
+        //         <Chart options={options} type="bar" series={series} width="100%" />
+        //     </div> */}
+        //     </div> : "Select a state to show table data"}
+        // </div>
+        <TableContainer component={Paper}>
+          <Table aria-aria-label='simple table'>
+            <TableHead>
+              <TableRow>
+                <TableCell>District</TableCell>
+                <TableCell>Incumbent</TableCell>
+                <TableCell>Party</TableCell>
+                <TableCell>2022 W/L</TableCell>
+                <TableCell>Geographic Variation</TableCell>
+                <TableCell>Population Variation</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow></TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       );
     }
 
