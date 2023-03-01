d3f66f64b7bf77dc051a81f469c49ef8aee18fc2
Nazif Mahamud <mamun01188@gmail.com>
2023-03-01 01:05:13 -0500

    Hey it expands

--- client/src/components/TableComponent.js
+++ client/src/components/TableComponent.js
@@ -14,19 +14,22 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 function TableComponent() {
 
     const map = useSelector(state => state.map)
     const dispatch = useDispatch();
 
-    const tableCellClickHandler = (e) => {
-      console.log(e.target);
+    const tableCellClickHandler = (e, district) => {
+      console.log(district);
+      dispatch(setSelectedDistrict(district));
     };
     
     let rows = [];
     if (map.currentGraphData != null) {
-        rows = map.currentGraphData.incumbents2022.map(x => [x.district, x.winner, x.votes, x.house]);
+        rows = map.currentGraphData.incumbents2022.map(x => [x.district, x.winner, x.votes, x.house, x.loser, x.loserhouse, x.loservotes]);
     } // Should grab all the winner data per district.
     // console.log(rows);
     
@@ -49,7 +52,7 @@ function TableComponent() {
               <TableBody>
                 {rows.map((row) => (
                   // console.log(row),
-                  <TableRow onClick={tableCellClickHandler} key={row[0]}
+                  <TableRow onClick={(event)=>tableCellClickHandler(event, row[0])} key={row[0]}
                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                     <TableCell component="th">{row[0]}</TableCell>
                     <TableCell>{row[1]}</TableCell>
@@ -65,7 +68,59 @@ function TableComponent() {
         );
       }
       else {
-
+        return (
+          <Box>
+          <TableContainer component={Paper} sx={{ maxHeight: '42vh' }}>
+            <Table aria-aria-label='simple table'>
+              <TableHead>
+                <TableRow>
+                  <TableCell>District</TableCell>
+                  <TableCell>Incumbent</TableCell>
+                  <TableCell>Party</TableCell>
+                  <TableCell>2022 W/L</TableCell>
+                  <TableCell>Geographic Variation</TableCell>
+                  <TableCell>Population Variation</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow key={map.currentDistrict-1}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell component="th">{rows[map.currentDistrict-1][0]}</TableCell>
+                    <TableCell>{rows[map.currentDistrict-1][1]}</TableCell>
+                    <TableCell>{rows[map.currentDistrict-1][3]}</TableCell>
+                    <TableCell>Winner</TableCell>
+                    <TableCell>Dummy Data</TableCell>
+                    <TableCell>Dummy Data</TableCell>
+                </TableRow>
+                {rows[map.currentDistrict-1][4].map((incumbent, index) => (
+                  <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
+                    <TableCell>{map.currentDistrict}</TableCell>
+                    <TableCell>{incumbent}</TableCell>
+                    <TableCell>{rows[map.currentDistrict-1][5][index]}</TableCell>
+                    <TableCell>Loser</TableCell>
+                    <TableCell>Dummy Data</TableCell>
+                    <TableCell>Dummy Data</TableCell>
+                  </TableRow>
+                ))}
+                {/* {rows[map.currentDistrict-1].map((row) => (
+                  // console.log(row),
+                  <TableRow onClick={(event)=>tableCellClickHandler(event, row[0])} key={row[0]}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell component="th">{row[0]}</TableCell>
+                    <TableCell>{row[1]}</TableCell>
+                    <TableCell>{row[3]}</TableCell>
+                    <TableCell>Loser</TableCell>
+                    <TableCell>Dummy Data</TableCell>
+                    <TableCell>Dummy Data</TableCell>
+                  </TableRow>
+                ))} */}
+              </TableBody>
+            </Table>
+            <Button variant="contained">Reset</Button>
+          </TableContainer>
+          
+          </Box>
+        );
       }
     }
       
