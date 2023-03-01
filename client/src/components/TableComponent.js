231bd10d61fd037e05edeab3cc96ca61db44a7b7
Nazif Mahamud <mamun01188@gmail.com>
2023-03-01 00:27:43 -0500

    working on expanding the graph

--- client/src/components/TableComponent.js
+++ client/src/components/TableComponent.js
@@ -12,20 +12,26 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
 
 function TableComponent() {
 
     const map = useSelector(state => state.map)
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+
+    const tableCellClickHandler = (e) => {
+      console.log(e.target);
+    };
     
     let rows = [];
     if (map.currentGraphData != null) {
         rows = map.currentGraphData.incumbents2022.map(x => [x.district, x.winner, x.votes, x.house]);
-    } // Should grab all the winner data per district. The console.log should verify it
-    console.log(rows);
+    } // Should grab all the winner data per district.
+    // console.log(rows);
     
     if (map.selectedState != null){
-      if (map.setSelectedDistrict == null) {
+      if (map.currentDistrict == null) {
         return (
   
           <TableContainer component={Paper} sx={{ maxHeight: '95vh' }}>
@@ -35,20 +41,20 @@ function TableComponent() {
                   <TableCell>District</TableCell>
                   <TableCell>Incumbent</TableCell>
                   <TableCell>Party</TableCell>
-                  {/* <TableCell>2022 W/L</TableCell> */}
+                  <TableCell>2022 W/L</TableCell>
                   <TableCell>Geographic Variation</TableCell>
                   <TableCell>Population Variation</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 {rows.map((row) => (
-                  console.log(row),
-                  <TableRow key={row[0]}
+                  // console.log(row),
+                  <TableRow onClick={tableCellClickHandler} key={row[0]}
                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                     <TableCell component="th">{row[0]}</TableCell>
                     <TableCell>{row[1]}</TableCell>
                     <TableCell>{row[3]}</TableCell>
-                    {/* <TableCell>{row[2]}</TableCell> */}
+                    <TableCell>Winner</TableCell>
                     <TableCell>Dummy Data</TableCell>
                     <TableCell>Dummy Data</TableCell>
                   </TableRow>
@@ -58,11 +64,14 @@ function TableComponent() {
           </TableContainer>
         );
       }
+      else {
+
       }
+    }
       
-      else{
-        return "Select a state to show table data";
-      }
+    else {
+      return "Select a state to show table data";
+    }
     }
 
 export default TableComponent;
