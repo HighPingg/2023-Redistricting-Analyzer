848b4a2b00fca4bb411f89af934712cd1f1abbfe
Vincent Zheng <zhengvincent0825@gmail.com>
2023-04-19 19:59:19 -0400

    finished getting neighbors precinct

--- preprocessing/transformData.py
+++ preprocessing/transformData.py
@@ -2,10 +2,21 @@ import geopandas as gpd
 import maup
 
 def initializeData(fileName):
-    df = gpd.read_file(fileName)   # Import GeoJSON
+    df = gpd.read_file(fileName)                        # Import GeoJSON
     df = df.to_crs(3857)                                # Convert CRS
     df['geometry'] = maup.close_gaps(df)                # Close gaps in GeoJSON
     df['geometry'] = maup.resolve_overlaps(df)          # Fix overlaps
+
+    return df
+
+def getNeighbors(df):
+    neighbors = []
+    geometries = df['geometry']
+    for shape in geometries:
+        neighbors.append(','.join(df[geometries.overlaps(shape.buffer(60.96))]['VTDST20'].tolist()))
+
+    return neighbors
+
 
 def exportToFile(df, fileName):
     df.to_file(fileName, driver="GeoJSON")
