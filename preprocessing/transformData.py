5c34f5c9f4584c9bbdea23f1c1b482659b54d5b4
Vincent Zheng <zhengvincent0825@gmail.com>
2023-04-19 21:55:35 -0400

    fixed neighbors function

--- preprocessing/transformData.py
+++ preprocessing/transformData.py
@@ -1,5 +1,7 @@
 import geopandas as gpd
-import maup
+# import maup
+
+from collections import defaultdict
 
 def initializeData(fileName):
     df = gpd.read_file(fileName)                        # Import GeoJSON
@@ -10,13 +12,32 @@ def initializeData(fileName):
     return df
 
 def getNeighbors(df):
-    neighbors = []
-    geometries = df['geometry']
-    for shape in geometries:
-        neighbors.append(','.join(df[geometries.overlaps(shape.buffer(60.96))]['VTDST20'].tolist()))
+    bufferedDF = df.copy()
+    bufferedDF['geometry'] = bufferedDF['geometry'].buffer(60.96)
 
-    return neighbors
-
+    # Iterate through all intersections and populate dictionary set
+    intersections = df.overlay(bufferedDF, how='intersection')
+    neighbors = defaultdict(set)
+    for i in range(intersections.shape[0]):
+        if intersections['VTDST20_1'][i] != intersections['VTDST20_2'][i]:
+            neighbors[intersections['VTDST20_1'][i]].add(intersections['VTDST20_2'][i])
+    
+    # Combine sets in dictionary to an array of neighbors
+    arr = []
+    for i in range(df.shape[0]):
+       arr.append(','.join(neighbors[df['VTDST20'][i]]))
+    
+    return arr
 
 def exportToFile(df, fileName):
     df.to_file(fileName, driver="GeoJSON")
+
+df = gpd.read_file('GeoJSON/OH_PRECINCTS_FIXED.json')
+df['NEIGHBORS'] = getNeighbors(df)
+
+print(df['NEIGHBORS'].to_string())
+#079AEM
+# vid = df['VTDST20'].tolist()
+# neighbors = df['NEIGHBORS'].tolist()
+# for v, n in zip(vid, neighbors):
+#     print(v, n)
