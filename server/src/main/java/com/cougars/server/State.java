309ee42217545d0179a7a242a94c1444e525c28d
Dylan Gold <dylangoldj@gmail.com>
2023-04-21 21:57:07 -0400

    Fixed missing sets that were arrays

--- server/src/main/java/com/cougars/server/State.java
+++ server/src/main/java/com/cougars/server/State.java
@@ -1,20 +1,20 @@
 package com.cougars.server;
 import lombok.Data;
 
-import java.util.ArrayList;
+import java.util.Set;
 @Data
 public class State{
     private String name;
-    private ArrayList<District> districts;
+    private Set<DistrictPlan> districts;
     private Ensemble ensemble;
 
-    public State(String name, ArrayList<District> districts, Ensemble ensemble) {
+    public State(String name, Set<DistrictPlan> districts, Ensemble ensemble) {
         this.name = name;
         this.districts = districts;
         this.ensemble = ensemble;
     }
 
-    public State(String name, ArrayList<District> districts){
+    public State(String name, Set<DistrictPlan> districts){
         this.name = name;
         this.districts = districts;
     }
--- server/src/main/java/com/cougars/server/StateConfig.java
+++ server/src/main/java/com/cougars/server/StateConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+
 
 @Configuration
 @ComponentScan("com.cougars.server")
@@ -13,19 +15,19 @@ public class StateConfig {
     @Bean
     @Qualifier("Ohio")
     public State Ohio() {
-        return new State("Ohio", new ArrayList<>());
+        return new State("Ohio", new HashSet<>());
     }
 
     @Bean
     @Qualifier("Nevada")
     public State Nevada() {
         Ensemble ensemble = new Ensemble(1, 2, 3, 1.0, 2.0, "hello1", "Testing2", "3x", "4x", "5x");
-        return new State("Nevada", new ArrayList<>(), ensemble);
+        return new State("Nevada", new HashSet<>(), ensemble);
     }
 
     @Bean
     @Qualifier("Illinois")
     public State Illinois() {
-        return new State("Illinois", new ArrayList<>());
+        return new State("Illinois", new HashSet<>());
     }
 }
