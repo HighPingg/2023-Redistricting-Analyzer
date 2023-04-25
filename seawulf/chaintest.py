c6ade1c2bc1d56c8ab55a642c758919973d2d2f7
Nazif Mahamud <nazif.mahamud1213@gmail.com>
2023-04-25 01:55:53 -0400

    We actually got a graph so we can do MGGG

--- seawulf/chaintest.py
+++ seawulf/chaintest.py
@@ -2,9 +2,9 @@ import geopandas as gpd
 from gerrychain import GeographicPartition, Partition, Graph, MarkovChain, proposals, updaters, constraints, accept, Election
 from gerrychain.proposals import flip, recom
 import pandas
-import numpy
+import numpy as np
 
-shapefile = gpd.read_file(r'C:\Users\mamun\Documents\2023-Redistricting-Analyser-Seawulf\nv test\tl_2020_32_cd116.shp')
+shapefile = gpd.read_file(r'OhioPrecincts.geojson')
 # shapefile = gpd.read_file(r'C:\Users\mamun\Documents\2023-Redistricting-Analyser-Seawulf\Nevada 2020\NV_PRECINCTS.geojson')
 # shapefile = gpd.read_file(r'C:\Users\mamun\Documents\2023-Redistricting-Analyser-Seawulf\test.json')
 # shapefile['id']=numpy.arange(start=0, stop=2094,step=1)
@@ -13,33 +13,34 @@ shapefile = gpd.read_file(r'C:\Users\mamun\Documents\2023-Redistricting-Analyser
 #     setattr(i, 'id', id)
 # #     # i.id = id
 #     id+=1
+shapefile['geometry'] = shapefile['geometry'].buffer(0.001)
 
 # graph = Graph.from_file(r'C:\Users\mamun\Documents\2023-Redistricting-Analyser-Seawulf\Nevada 2020\NV_PRECINCTS.geojson', ignore_errors=True)
-graph = Graph.from_geodataframe(shapefile, ignore_errors=True)
+graph = Graph.from_geodataframe(shapefile)
 
-num_districts = 4
+# num_districts = 4
 
-elections = [Election("G20PRE",{"Democratic":"G20PREDBID", "Republican":"G20PRERTRU", "Libertarian": "G20PRELJOR", "Independent American":"G20PREIBLA", "None":"G20PREONON"})]
+# elections = [Election("G20PRE",{"Democratic":"G20PREDBID", "Republican":"G20PRERTRU", "Libertarian": "G20PRELJOR", "Independent American":"G20PREIBLA", "None":"G20PREONON"})]
 
-init_assignment = {i: i % num_districts for i in shapefile.index}
+# init_assignment = {i: i % num_districts for i in shapefile.index}
 
-partition = GeographicPartition(shapefile, init_assignment)
+# partition = GeographicPartition(shapefile, init_assignment)
 
-proposal = flip
+# proposal = flip
 
-acceptance = accept.cut_edge_accept
+# acceptance = accept.cut_edge_accept
 
-chain = MarkovChain(
-    proposal=proposal,
-    constraints=None,
-    accept=acceptance,
-    initial_state=partition,
-    total_steps=1000
-)
+# chain = MarkovChain(
+#     proposal=proposal,
+#     constraints=None,
+#     accept=acceptance,
+#     initial_state=partition,
+#     total_steps=1000
+# )
 
-for partition in chain:
-    pass
+# for partition in chain:
+#     pass
 
-shapefile['district'] = [partition.assignment[i] for i in shapefile.index]
+# shapefile['district'] = [partition.assignment[i] for i in shapefile.index]
 
-shapefile.plot(column='district', cmap='tab10')
+# shapefile.plot(column='district', cmap='tab10')
