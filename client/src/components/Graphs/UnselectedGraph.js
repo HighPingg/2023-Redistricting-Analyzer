c785665f66a8a37cf02d5d31284fce233210bf2f
Dylan Gold <dylangoldj@gmail.com>
2023-05-11 15:09:44 -0400

    Made unselected graph box look nice

---
+++ client/src/components/Graphs/UnselectedGraph.js
@@ -0,0 +1,26 @@
+import Box from '@mui/material/Box';
+
+function UnselectedGraph(){
+    return (
+        <Box
+				display="flex"
+				justifyContent= 'center'
+				alignItems= 'center'
+				sx={{
+					height: '100%',
+					width: '100%',
+					borderRadius: 2,
+					backgroundColor: '#aaacad',
+					fontSize: 50,
+					fontWeight: 'bold'
+				}}
+        >
+						<Box
+						>
+						Select Graph Type to Display
+						</Box>
+        </Box>
+      );
+}
+
+export default UnselectedGraph
