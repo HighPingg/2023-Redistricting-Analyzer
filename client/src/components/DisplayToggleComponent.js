ab9fa40da2c8204ffc425e99bbf011b89e17f1b9
Nazif Mahamud <nazif.mahamud1213@gmail.com>
2023-05-16 05:21:48 -0400

    Grayed out 2020?

--- client/src/components/DisplayToggleComponent.js
+++ client/src/components/DisplayToggleComponent.js
@@ -20,6 +20,37 @@ function DisplayToggle(){
   }
 
   if (map.selectedState!= null){
+    if (map.currentDistrictPlan == '2020') {
+      return(
+        <Box
+        sx = {{pb: '.5%'}}
+        >
+          <ToggleButtonGroup
+            color="primary"
+            // value={map.currentDisplay}
+            exclusive
+            // onChange={selectDisplayChange}
+            aria-label="Platform"
+            sx={{
+              marginTop: '10px',
+            }}
+          >
+            <ToggleButton disabled={true} value="Population" sx={buttonStyle}>
+              Population Variation
+              </ToggleButton>
+            <ToggleButton disabled={true} value="BoxWhisker" sx={buttonStyle}>
+              Geometric Variation
+              </ToggleButton>
+            <ToggleButton disabled={true}  value="SplitParty" sx={buttonStyle}>
+              Split Party View
+              </ToggleButton>
+            <ToggleButton disabled={true}  value="Demographic"sx={buttonStyle}>
+              Demographic
+              </ToggleButton>
+          </ToggleButtonGroup>
+          </Box>
+        )
+    }
   return(
     <Box
     sx = {{pb: '.5%'}}
